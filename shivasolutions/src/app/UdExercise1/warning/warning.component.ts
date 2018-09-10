15d867554ec5572122ea58b05fff5df330f960c7
sumant <sumant1801@gmail.com>
2018-09-09 22:01:44 -0400

    First Exercise

---
+++ shivasolutions/src/app/UdExercise1/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component ({
+selector: 'warning-message',
+templateUrl: './warning.component.html',
+styleUrls: ['./warning.component.css']
+})
+
+export class WarningComponent{
+
+}
--- shivasolutions/src/app/app.module.ts
+++ shivasolutions/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './UdExercise1/warning/warning.component';
+import { SuccessComponent } from './UdExercise1/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
