03a94926d7888a422a3d8f0a7eab98425b0e6d01
sumant <sumant1801@gmail.com>
2018-09-09 20:13:58 -0400

    component class selector

--- shivasolutions/src/app/servers/servers.component.ts
+++ shivasolutions/src/app/servers/servers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  selector: '.app-servers', //compnenet class selector
   // templateUrl: './servers.component.html',
   template: `<app-server></app-server>
   <app-server></app-server>`,
