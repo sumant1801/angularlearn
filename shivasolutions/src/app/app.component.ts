875a4d98824bf47fc7ad92557ac4dafedba81663
sumant <sumant1801@gmail.com>
2018-09-09 20:04:22 -0400

    Style tutorial

--- shivasolutions/src/app/app.component.ts
+++ shivasolutions/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color: darkred
+  }
+  `]
 })
 export class AppComponent {
   title = 'shivasolutions - A solution for Advertisment and finance technology';
